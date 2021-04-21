ca35d223bf895ba3732c43a392a0ad10d317e2e6
Marvin Murmann <m.murmann@nextint.de>
2021-04-21 16:37:49 +0200

    nav js comments / cleanup

--- public/js/app.js
+++ public/js/app.js
@@ -1845,30 +1845,43 @@ __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 var $ = __webpack_require__(/*! jquery */ "./node_modules/jquery/dist/jquery.js");
 
 $(function () {
-  $('.arrow').on('click', function () {
-    if ($('nav').hasClass('open')) {
-      $('.cover').css('display', 'none');
-    } else {
-      $('.cover').css('display', 'block');
-    }
+  // navigation
+  $('nav').on('click', function (e) {
+    // prevent nav closing on menu interaction
+    var menus = $('nav ul li');
 
-    $('nav').toggleClass('open');
-    $('.nav-list').slideToggle(750);
-    $('.arrow').toggleClass('arrow-transformed');
-  });
+    if (!menus.is(e.target)) {
+      // check menu status
+      if ($('nav').hasClass('open')) {
+        $('.cover').hide();
+      } else {
+        $('.cover').show();
+      } // change menu status
+
+
+      $('nav').toggleClass('open'); // close menus if appropriate
+
+      $('.nav-list').slideToggle(750); // play arrow animation
+
+      $('.arrow').toggleClass('arrow-transformed');
+    }
+  }); // menu functionality
+
   $('.menu-main').on('click', function () {
     $(this).children('ul').slideToggle(750);
-  });
+  }); // nav closing functionality
+
   $('.cover').on('click', function () {
     $('.cover').css('display', 'none');
     $('nav').toggleClass('open');
     $('.nav-list').slideToggle(750);
     $('.arrow').toggleClass('arrow-transformed');
-  });
-  $(document).on('keydown', function (e) {
-    console.log(e.key);
+  }); // check for keyevent
 
+  $(document).on('keydown', function (e) {
+    // check for specific key
     if (e.key == "Escape") {
+      // close menu if open
       if ($('nav').hasClass('open')) {
         $('.cover').css('display', 'none');
         $('nav').toggleClass('open');
--- resources/js/app.js
+++ resources/js/app.js
@@ -2,30 +2,43 @@ require('./bootstrap');
 var $ = require('jquery');
 
 $(function(){
-    $('.arrow').on('click', function(){
-        if($('nav').hasClass('open')){
-            $('.cover').css('display', 'none');
-        }
-        else{
-            $('.cover').css('display', 'block');
-        }
-        $('nav').toggleClass('open');
-        $('.nav-list').slideToggle(750);
-
-        $('.arrow').toggleClass('arrow-transformed');
+    // navigation
+    $('nav').on('click', function(e){
+        // prevent nav closing on menu interaction
+        let menus = $('nav ul li');
+        if(!(menus.is(e.target))){
+            // check menu status
+            if($('nav').hasClass('open')){
+                $('.cover').hide();
+            }
+            else{
+                $('.cover').show();
+            }
+            // change menu status
+            $('nav').toggleClass('open');
+            // close menus if appropriate
+            $('.nav-list').slideToggle(750);
+            // play arrow animation
+            $('.arrow').toggleClass('arrow-transformed');
+        }        
     })
+    // menu functionality
     $('.menu-main').on('click', function(){
         $(this).children('ul').slideToggle(750);
     })
+
+    // nav closing functionality
     $('.cover').on('click', function(){
         $('.cover').css('display', 'none');
         $('nav').toggleClass('open');
         $('.nav-list').slideToggle(750);
         $('.arrow').toggleClass('arrow-transformed');
     })
+    // check for keyevent
     $(document).on('keydown', function(e){
-        console.log(e.key);
+        // check for specific key
         if(e.key == "Escape"){
+            // close menu if open
             if($('nav').hasClass('open')){
                 $('.cover').css('display', 'none');
                 $('nav').toggleClass('open');
